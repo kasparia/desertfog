8b3ef930b0d46459d4f41243488e36513c5d27ec
kasparia <superkasperi@gmail.com>
2020-11-15 11:21:44 -0800

    Add water fx and state of glitch fx

--- src/components/Boxery.jsx
+++ src/components/Boxery.jsx
@@ -1,18 +1,18 @@
 import React, { useRef } from 'react';
-import { useFrame } from 'react-three-fiber';
+import { useFrame, extend } from 'react-three-fiber';
 
 export const Boxery = ({position}) => {
-    const mesh = useRef(null);
+    const ref = useRef();
     let tick = 0.01;
     let meshPosition = position;
 
     useFrame(() => {
-        mesh.current.rotation.x += 0.01;
-        mesh.current.rotation.y += 0.01;
+        ref.current.rotation.x += 0.01;
+        ref.current.rotation.y += 0.01;
 
         tick += 0.01;
         meshPosition =  [
-            Math.sin(mesh.current.position.x * tick ) * 1,
+            Math.sin(ref.current.position.x * tick ) * 1,
             -5,
             -1
         ]        
@@ -20,7 +20,7 @@ export const Boxery = ({position}) => {
 
     return (
         <mesh
-            ref={ mesh }
+            ref={ ref }
             position={ meshPosition }
             castShadow
         >
--- src/components/Effects.jsx
+++ src/components/Effects.jsx
@@ -1,13 +1,15 @@
 import * as THREE from 'three'
-import React, { useRef, useMemo, useEffect } from 'react'
+import React, { useRef, useState, useMemo, useEffect } from 'react'
 import { extend, useThree, useFrame } from 'react-three-fiber'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass'
+import { GlitchPass } from './shaders/Glitchpass';
+import { WaterPass } from './shaders/Waterpass';
 
-extend({ EffectComposer, ShaderPass, RenderPass, UnrealBloomPass, FilmPass })
+extend({ EffectComposer, ShaderPass, RenderPass, UnrealBloomPass, FilmPass, GlitchPass, WaterPass })
 
 export const Effects = ({ down }) => {
   const composer = useRef()
@@ -17,17 +19,21 @@ export const Effects = ({ down }) => {
     return new THREE.Vector2(512, 512);
   }, []);
 
-  let filmRatio = 0.62;
-  let filmLines = 75;
+  let filmLines = 45;
+  let glitchTick = 0;
+
+  let [glitchToggle, setToggle] = useState(false);
 
   useEffect(() => {
     void composer.current.setSize(size.width, size.height);
   }, [size]);
 
   useFrame(({clock}) => {
-    //filmRatio = Math.abs(0.3 + Math.sin(clock.getElapsedTime()) * 0.25);
-    filmLines = Math.abs(80 + Math.sin(clock.getElapsedTime()) * 25);
-    console.log(filmLines);
+    glitchTick = Math.abs(Math.sin(clock.getElapsedTime()));
+    //glitchToggle = ~~(glitchTick > 0.8);
+    setToggle(~~(glitchTick > 0.8));
+    console.log(glitchToggle);
+
     return composer.current.render()
   }, 1);
 
@@ -35,7 +41,9 @@ export const Effects = ({ down }) => {
     <effectComposer ref={composer} args={[gl]}>
       <renderPass attachArray='passes' scene={scene} camera={camera} />
       <unrealBloomPass attachArray='passes' args={[aspect, 1.5, 1, 0]} />
-      <filmPass attachArray='passes' args={[0.1, filmRatio, filmLines, false]} />
+      <filmPass attachArray='passes' args={[0.1, 0.62, filmLines, false]} />
+      <glitchPass attachArray='passes' factor={ glitchToggle } />
+      <waterPass attachArray="passes" factor={1.5} />
     </effectComposer>
   )
 }
